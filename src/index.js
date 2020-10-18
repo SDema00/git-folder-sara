bc58d3dbf31ca69cb47d2470c1819e81388e71ab
SDema00 <46246764+SDema00@users.noreply.github.com>
2020-10-18 18:37:42 -0400

    created folders

---
+++ src/index.js
@@ -0,0 +1,45 @@
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+let currentDate = document.querySelector("#current-date");
+let day = days[now.getDay()];
+let hour = now.getHours();
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+console.log(hour);
+console.log(minutes);
+console.log(day);
+
+currentDate.innerHTML = `${day}, ${hour}:${minutes}`;
+
+//2
+function displayWeatherCondition(response) {
+  console.log(response);
+  document.querySelector("#current-city").innerHTML = response.data.name;
+  document.querySelector("#current-temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#current-humidity").innerHTML =
+    response.data.main.humidity;
+}
+
+function inputCity(event) {
+  event.preventDefault();
+  let apiKey = "3cbe34899ba9cdaea2a1be7e83c6edee";
+  let currentCity = document.querySelector("#search-city").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", inputCity);
