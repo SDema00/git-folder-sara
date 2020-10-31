54cbe6206ea5e94585095642b8363c8129175426
SDema00 <46246764+SDema00@users.noreply.github.com>
2020-10-31 11:51:43 -0400

    added automated city

--- src/index.js
+++ src/index.js
@@ -30,14 +30,22 @@ function displayWeatherCondition(response) {
     iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`)
 }
 
-function inputCity(event) {
-  event.preventDefault();
+
+function search(city) {
   let apiKey = "3cbe34899ba9cdaea2a1be7e83c6edee";
-  let currentCity = document.querySelector("#search-city").value;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${currentCity}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
   axios.get(apiUrl).then(displayWeatherCondition);
 }
 
+
+function handleSubmit(event) {
+  event.preventDefault();
+ let currentCity = document.querySelector("#search-city").value;  
+ search(currentCity.value)
+
+}
+
 let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", inputCity);
+searchForm.addEventListener("submit", handleSubmit);
+
+search("Washington DC");
